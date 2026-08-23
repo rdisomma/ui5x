1009fce76c52ce6fa395d74938ddc49536620887
raffaeledisomma <raffaeledisomma.develop@gmail.com>
2026-08-23 16:45:58 +0200

    chore(types): refresh generated UI5 declarations

--- src/ui5x/loading/Skeleton.gen.d.ts
+++ src/ui5x/loading/Skeleton.gen.d.ts
@@ -50,36 +50,62 @@ declare module "./Skeleton" {
         // property: width
 
         /**
+         * Gets current value of property "width".
+         *
          * Defines the width of the skeleton.
         
         Accepts any valid UI5 CSSSize value, such as
         "100%", "20rem" or "320px".
+         *
+         * @returns Value of property "width"
          */
         getWidth(): CSSSize;
 
         /**
+         * Sets a new value for property "width".
+         *
          * Defines the width of the skeleton.
         
         Accepts any valid UI5 CSSSize value, such as
         "100%", "20rem" or "320px".
+         *
+         * When called with a value of "null" or "undefined", the default value of the property will be restored.
+         *
+         * @param width New value for property "width"
+         * @returns Reference to "this" in order to allow method chaining
          */
         setWidth(width: CSSSize): this;
 
         // property: type
 
         /**
+         * Gets current value of property "type".
+         *
          * Defines the visual shape of the skeleton.
+         *
+         * Default value is: "SkeletonType.Line"
+         * @returns Value of property "type"
          */
         getType(): SkeletonType;
 
         /**
+         * Sets a new value for property "type".
+         *
          * Defines the visual shape of the skeleton.
+         *
+         * When called with a value of "null" or "undefined", the default value of the property will be restored.
+         *
+         * Default value is: "SkeletonType.Line"
+         * @param [type="SkeletonType.Line"] New value for property "type"
+         * @returns Reference to "this" in order to allow method chaining
          */
         setType(type: SkeletonType): this;
 
         // property: lines
 
         /**
+         * Gets current value of property "lines".
+         *
          * /**
                      * Defines the number of placeholder lines.
                      *
@@ -89,10 +115,15 @@ declare module "./Skeleton" {
         .
                      *
                      * Valid values are between 1 and 3.
+         *
+         * Default value is: 1
+         * @returns Value of property "lines"
          */
         getLines(): number;
 
         /**
+         * Sets a new value for property "lines".
+         *
          * /**
                      * Defines the number of placeholder lines.
                      *
@@ -102,24 +133,43 @@ declare module "./Skeleton" {
         .
                      *
                      * Valid values are between 1 and 3.
+         *
+         * When called with a value of "null" or "undefined", the default value of the property will be restored.
+         *
+         * Default value is: 1
+         * @param [lines=1] New value for property "lines"
+         * @returns Reference to "this" in order to allow method chaining
          */
         setLines(lines: number): this;
 
         // property: animated
 
         /**
+         * Gets current value of property "animated".
+         *
          * Defines whether the loading animation is enabled.
         
         The animation is automatically disabled when the user's
         operating system requests reduced motion.
+         *
+         * Default value is: true
+         * @returns Value of property "animated"
          */
         getAnimated(): boolean;
 
         /**
+         * Sets a new value for property "animated".
+         *
          * Defines whether the loading animation is enabled.
         
         The animation is automatically disabled when the user's
         operating system requests reduced motion.
+         *
+         * When called with a value of "null" or "undefined", the default value of the property will be restored.
+         *
+         * Default value is: true
+         * @param [animated=true] New value for property "animated"
+         * @returns Reference to "this" in order to allow method chaining
          */
         setAnimated(animated: boolean): this;
     }
